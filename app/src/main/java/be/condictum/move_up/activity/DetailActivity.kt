4c50df23b8c0b4b9c26bb9ddfbb9fe5ae7d5f1fd
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2021-07-28 22:34:51 +0300

    bottom nav view is fixed.

--- app/src/main/java/be/condictum/move_up/activity/DetailActivity.kt
+++ app/src/main/java/be/condictum/move_up/activity/DetailActivity.kt
@@ -1,17 +1,33 @@
 package be.condictum.move_up.activity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import be.condictum.move_up.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val selectedFragment =
-            supportFragmentManager.findFragmentById(R.id.detail_nav_host_fragment) as NavHostFragment
-        val navController = selectedFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.detail_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.goalScreenFragment, R.id.profileFragment, R.id.settingsFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setOnItemSelectedListener {item ->
+            onNavDestinationSelected(item, Navigation.findNavController(this, R.id.detail_nav_host_fragment))
+        }
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
