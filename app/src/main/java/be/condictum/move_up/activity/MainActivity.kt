7e2682b31a00c0278871cede081fa9a62eb292a2
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2021-07-28 14:47:03 +0300

    fixed main activity

--- app/src/main/java/be/condictum/move_up/activity/MainActivity.kt
+++ app/src/main/java/be/condictum/move_up/activity/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.navigation.fragment.NavHostFragment
 import be.condictum.move_up.R
 
 class MainActivity : AppCompatActivity() {
-    private val navHostFragment =
-        supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
-    val navController = navHostFragment.navController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectedFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+                    as NavHostFragment?
+        val navController = selectedFragment?.navController
     }
 }
