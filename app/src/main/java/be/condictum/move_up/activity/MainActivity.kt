87185ab72eaf94f0b05cd2bd22e1350899750635
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2021-07-29 10:35:32 +0300

    code refactor

--- app/src/main/java/be/condictum/move_up/activity/MainActivity.kt
+++ app/src/main/java/be/condictum/move_up/activity/MainActivity.kt
@@ -1,17 +1,60 @@
 package be.condictum.move_up.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import be.condictum.move_up.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val selectedFragment =
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
-        val navController = selectedFragment.navController
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.goalScreenFragment,
+                R.id.profileFragment,
+                R.id.settingsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            NavigationUI.onNavDestinationSelected(
+                item,
+                Navigation.findNavController(this, R.id.main_nav_host_fragment)
+            )
+        }
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.goalScreenFragment -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.mainFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                R.id.splashScreenFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/be/condictum/move_up/fragment/MainFragment.kt
+++ app/src/main/java/be/condictum/move_up/fragment/MainFragment.kt
@@ -1,14 +1,13 @@
 package be.condictum.move_up.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import be.condictum.move_up.R
-import be.condictum.move_up.activity.DetailActivity
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -48,9 +47,8 @@ class MainFragment : Fragment() {
         val button = view.findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
-            val intent = Intent(activity, DetailActivity::class.java)
-            startActivity(intent)
-            activity?.finish()
+            val action = MainFragmentDirections.actionMainFragmentToGoalScreenFragment()
+            view.findNavController().navigate(action)
         }
     }
 
