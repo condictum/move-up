d187c167f0ee5f51b0c86c9ef816157cafcd71f0
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2021-07-29 15:46:29 +0300

    fixed

--- app/src/main/java/be/condictum/move_up/fragment/SplashScreenFragment.kt
+++ app/src/main/java/be/condictum/move_up/fragment/SplashScreenFragment.kt
@@ -1,14 +1,16 @@
 package be.condictum.move_up.fragment
 
 import android.animation.ObjectAnimator
-import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import be.condictum.move_up.R
 import be.condictum.move_up.databinding.FragmentSplashScreenBinding
 
 class SplashScreenFragment : Fragment() {
@@ -30,29 +32,42 @@ class SplashScreenFragment : Fragment() {
             duration = 2000
             start()
         }
+
         ObjectAnimator.ofFloat(binding.imageView2, "translationY", -100f).apply {
             duration = 2000
             start()
         }
+
         ObjectAnimator.ofFloat(binding.imageView2, "translationX", 100f).apply {
             duration = 2000
             start()
         }
+
         val c = Runnable {
             val action = SplashScreenFragmentDirections.actionSplashScreenFragmentToMainFragment()
             view.findNavController().navigate(action)
         }
 
         val textc = Runnable {
-            binding.textView2.text = "by condictum"
+            binding.textView2.text = getString(R.string.by_condictum_text)
         }
 
         val colorc = Runnable {
-            binding.layoutId.setBackgroundColor(Color.parseColor("#ffecfc"))
-            binding.imageView2.setBackgroundColor(Color.parseColor("#ffecfc"))
+            binding.layoutId.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.primary_color
+                )
+            )
+            binding.imageView2.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.primary_color
+                )
+            )
         }
 
-        val hand = Handler()
+        val hand = Handler(Looper.getMainLooper())
         hand.postDelayed(c, 3000)
         hand.postDelayed(textc, 1500)
         hand.postDelayed(colorc, 1500)
