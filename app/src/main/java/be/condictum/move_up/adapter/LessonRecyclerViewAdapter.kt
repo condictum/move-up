3918bfa4860e1b18f565830a3c2af96142dc50db
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2021-08-05 15:10:18 +0300

    goal screen add and list operations

--- app/src/main/java/be/condictum/move_up/adapter/LessonRecyclerViewAdapter.kt
+++ app/src/main/java/be/condictum/move_up/adapter/LessonRecyclerViewAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import be.condictum.move_up.R
 import be.condictum.move_up.database.data.Lessons
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class LessonRecyclerViewAdapter(private val mContext: Context, private var data: List<Lessons>) :
     RecyclerView.Adapter<LessonRecyclerViewAdapter.LessonsViewHolder>() {
@@ -16,6 +18,7 @@ class LessonRecyclerViewAdapter(private val mContext: Context, private var data:
         val lessonNameTextView =
             view.findViewById<TextView>(R.id.lessons_list_lesson_name_item_text)
         val lessonScoreTextView = view.findViewById<TextView>(R.id.lessons_list_score_item_text)
+        val lessonCardView = view.findViewById<CardView>(R.id.lessons_list_card_view_item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LessonsViewHolder {
@@ -28,8 +31,42 @@ class LessonRecyclerViewAdapter(private val mContext: Context, private var data:
     override fun onBindViewHolder(holder: LessonsViewHolder, position: Int) {
         val lesson = data[position]
 
-        holder.lessonNameTextView.text = lesson.lessonName
-        holder.lessonScoreTextView.text = lesson.lessonScore.toString()
+        var lessonName = lesson.lessonName
+        var lessonScore = lesson.lessonScore.toString()
+
+        if (lessonName.length > 8) {
+            lessonName = lessonName.substring(0, 8) + "..."
+        }
+
+        if (lessonScore.length > 7) {
+            lessonScore = lessonScore.substring(0, 7) + "..."
+        }
+
+        holder.lessonNameTextView.text =
+            mContext.getString(R.string.formatted_lesson_text, lessonName)
+        holder.lessonScoreTextView.text =
+            mContext.getString(R.string.formatted_score_text, lessonScore)
+
+        holder.lessonCardView.setOnClickListener {
+            val dialog = MaterialAlertDialogBuilder(mContext)
+
+            dialog.setTitle(mContext.getString(R.string.details_text))
+            dialog.setMessage(
+                "${
+                    mContext.getString(
+                        R.string.formatted_lesson_text,
+                        lesson.lessonName
+                    )
+                }\n${
+                    mContext.getString(
+                        R.string.formatted_score_text,
+                        lesson.lessonScore.toString()
+                    )
+                }"
+            )
+            dialog.setNegativeButton(mContext.getString(R.string.exit_button_text)) { _, _ -> }
+            dialog.show()
+        }
     }
 
     override fun getItemCount(): Int {
