a22f3e21429d60018245a8a01243ca6d6bd33b72
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2021-08-11 14:00:38 +0300

    fix

--- app/src/main/java/be/condictum/move_up/fragment/CountDownTimerScreenFragment.kt
+++ app/src/main/java/be/condictum/move_up/fragment/CountDownTimerScreenFragment.kt
@@ -1,112 +1,104 @@
 package be.condictum.move_up.fragment
 
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import be.condictum.move_up.databinding.FragmentCountDownTimerScreenBinding
 import io.paperdb.Paper
 
-import android.widget.Toast
-import cn.iwgang.countdownview.CountdownView
-import kotlin.concurrent.timer
-
-
 class CountDownTimerScreenFragment : Fragment() {
-
-    private val LIMIT_TIME: Long= 10000
-    var isStart = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-
-        }
+    companion object {
+        private const val IS_START_KEY = "IS_START"
+        private const val LAST_TIME_SAVED_KEY = "LAST_TIME_SAVED"
+        private const val TIME_REMAIN = "TIME_REMAIN"
     }
+
+    private var _binding: FragmentCountDownTimerScreenBinding? = null
+    private val binding get() = _binding!!
+
+    private val LIMIT_TIME: Long = 10000
+    var isStart = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(be.condictum.move_up.R.layout.fragment_count_down_timer_screen, container, false)
-
-        init()
+        _binding = FragmentCountDownTimerScreenBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        private fun init(){
-            val btn = requireView()!!.findViewById<Button>(be.condictum.move_up.R.id.cronoBtn)
-            val countdown_view = requireView()!!.findViewById<CountdownView>(be.condictum.move_up.R.id.cronometer)
+        initTimer()
+    }
+
+    private fun initTimer() {
+        val btn = binding.cronoBtn
+        val countdownView = binding.cronometer
+
         Paper.init(this.context)
-        isStart = Paper.book().read(IS_START_KEY,false)
-        if (isStart){
-           btn.isEnabled = false
+        isStart = Paper.book().read(IS_START_KEY, false)
+
+        if (isStart) {
+            btn.isEnabled = false
             checktime()
+        } else {
+            btn.isEnabled = true
         }
-            else{
-                btn.isEnabled=true
+
+        btn.setOnClickListener {
+            if (!isStart) {
+                countdownView.start(LIMIT_TIME)
+                Paper.book().write(IS_START_KEY, true)
             }
-            btn.setOnClickListener(){
+        }
 
-                if (!isStart){
-                    countdown_view.start(LIMIT_TIME)
-                    Paper.book().write(IS_START_KEY,true)
+        countdownView.setOnCountdownEndListener {
+            Toast.makeText(this.context, "finished", Toast.LENGTH_LONG).show()
+        }
 
-                }
-
-            }
-
-            countdown_view.setOnCountdownEndListener {
-                Toast.makeText(this.context,"finished",Toast.LENGTH_LONG).show()
-            }
-            countdown_view.setOnCountdownIntervalListener(1000,object:CountdownView.OnCountdownIntervalListener{
-                override fun onInterval(cv: CountdownView?, remainTime: Long) {
-                    Log.d("Tımer",""+remainTime)
-                }
-
-            })
+        countdownView.setOnCountdownIntervalListener(1000) { cv, remainTime ->
+            Log.d("Tımer", "" + remainTime)
+        }
     }
-    override fun onStop(){
-        val countdown_view = requireView()!!.findViewById<CountdownView>(be.condictum.move_up.R.id.cronometer)
-        Paper.book().write(LAST_TIME_SAVED_KEY,System.currentTimeMillis())
-        Paper.book().write(TIME_REMAIN,countdown_view.remainTime)
+
+    override fun onStop() {
         super.onStop()
+
+        val countdownView = binding.cronometer
+
+        Paper.book().write(LAST_TIME_SAVED_KEY, System.currentTimeMillis())
+        Paper.book().write(TIME_REMAIN, countdownView.remainTime)
     }
 
     private fun checktime() {
-        val countdown_view = requireView()!!.findViewById<CountdownView>(be.condictum.move_up.R.id.cronometer)
+        val countdownView = binding.cronometer
         val currentTime = System.currentTimeMillis()
-        val lastTimeSaved : Long = Paper.book().read(LAST_TIME_SAVED_KEY,0).toLong()
-        val timeRemain : Long = Paper.book().read(TIME_REMAIN,0).toLong()
-        val result = timeRemain + (lastTimeSaved-currentTime)
-        if (result>0){
-            countdown_view.start(result)
-        }
-        else{
-            countdown_view.stop()
+
+        val lastTimeSaved: Long = Paper.book().read(LAST_TIME_SAVED_KEY, 0).toLong()
+        val timeRemain: Long = Paper.book().read(TIME_REMAIN, 0).toLong()
+        val result = timeRemain + (lastTimeSaved - currentTime)
+
+        if (result > 0) {
+            countdownView.start(result)
+        } else {
+            countdownView.stop()
             reset()
         }
     }
 
     private fun reset() {
-        val btn = requireView()!!.findViewById<Button>(be.condictum.move_up.R.id.cronoBtn)
-        btn.isEnabled=true
+        val btn = binding.cronoBtn
+        btn.isEnabled = true
+        isStart = false
+
         Paper.book().delete(IS_START_KEY)
         Paper.book().delete(TIME_REMAIN)
         Paper.book().delete(LAST_TIME_SAVED_KEY)
-        isStart=false
-    }
-
-    companion object {
-        private const val IS_START_KEY = "IS_START"
-        private const val LAST_TIME_SAVED_KEY = "LAST_TIME_SAVED"
-        private const val TIME_REMAIN = "TIME_REMAIN"
     }
 }
