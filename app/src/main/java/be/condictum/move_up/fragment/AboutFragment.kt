b906cd2534d3731c7d680af326b86590acb6f73c
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2021-08-25 12:29:41 +0300

    added about menu and screen

---
+++ app/src/main/java/be/condictum/move_up/fragment/AboutFragment.kt
@@ -0,0 +1,30 @@
+package be.condictum.move_up.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import be.condictum.move_up.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setScreenTexts()
+    }
+
+    private fun setScreenTexts() {
+
+    }
+}
--- app/src/main/java/be/condictum/move_up/fragment/SettingsFragment.kt
+++ app/src/main/java/be/condictum/move_up/fragment/SettingsFragment.kt
@@ -1,36 +1,29 @@
 package be.condictum.move_up.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
 import be.condictum.move_up.R
 import be.condictum.move_up.database.DatabaseApplication
-import be.condictum.move_up.database.data.Goals
 import be.condictum.move_up.database.data.Settings
-import be.condictum.move_up.databinding.FragmentGoalScreenBinding
 import be.condictum.move_up.databinding.FragmentSettingsBinding
-import be.condictum.move_up.viewmodel.ProfilesViewModel
-import be.condictum.move_up.viewmodel.ProfilesViewModelFactory
 import be.condictum.move_up.viewmodel.SettingsViewModel
-import java.sql.Date
 
 
 class SettingsFragment : Fragment() {
-
-
-
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var data: List<Settings>
 
     private val viewModel: SettingsViewModel by activityViewModels {
         SettingsViewModel.SettingsViewModelFactory(
             (activity?.application as DatabaseApplication).database.settingsDao()
         )
     }
-private lateinit var data :List<Settings>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,17 +34,23 @@ private lateinit var data :List<Settings>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-        binding.kronometreSesiSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
-        binding.kronometreTitresimSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
+        setHasOptionsMenu(true)
     }
 
-    companion object {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        requireActivity().menuInflater.inflate(R.menu.about_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.ac_about -> {
+                val action =
+                    SettingsFragmentDirections.actionSettingsFragmentToAboutFragment()
+                Navigation.findNavController(requireView()).navigate(action)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
